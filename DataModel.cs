999429dc2db64de2be607376eab4a4f0801a039d
taopikrahman2110 <94630452+taopikrahman2110@users.noreply.github.com>
2024-05-03 14:26:58 +0700

    add data model

---
+++ DataModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRZReader
+{
+    public class DataModel
+    {
+        public string NomorPermohonan { get; set; }
+        public string NomorPaspor { get; set; }
+        public string NamaLengkap { get; set; }
+        public string TanggalLahir { get; set; }
+    }
+}
